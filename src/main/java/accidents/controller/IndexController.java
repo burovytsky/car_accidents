07b35d86fa59b6ac83d2c440102cb58f4fd29b5a
burovytsky <esfero1505@gmail.com>
2020-11-08 16:06:52 +0200

    1. Загрузка данных в вид [#330044]

--- src/main/java/accidents/controller/IndexController.java
+++ src/main/java/accidents/controller/IndexController.java
@@ -1,5 +1,6 @@
 package accidents.controller;
 
+import accidents.repository.AccidentMem;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
+        model.addAttribute("accidents", new AccidentMem().getAccidents());
         return "index";
     }
 }
---
+++ src/main/java/accidents/repository/AccidentMem.java
@@ -0,0 +1,30 @@
+package accidents.repository;
+
+import accidents.model.Accident;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class AccidentMem {
+    private final Map<Integer, Accident> accidents = new HashMap<>();
+
+    public AccidentMem() {
+        Accident accident1 = new Accident(0, "Accident1", "Description1", "some street");
+        Accident accident2 = new Accident(1, "Accident2", "Description2", "some street");
+        Accident accident3 = new Accident(2, "Accident3", "Description3", "some street");
+        accidents.put(accident1.getId(), accident1);
+        accidents.put(accident2.getId(), accident2);
+        accidents.put(accident3.getId(), accident3);
+    }
+
+    public Collection<Accident> getAccidents() {
+        return accidents.values();
+    }
+
+    public void createAccident(Accident accident) {
+        accidents.put(accident.getId(), accident);
+    }
+}
